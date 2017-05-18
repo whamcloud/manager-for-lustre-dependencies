d23ff4e0c6b86b0277e3f8f228b4819710b498b3
Brian J. Murrell <brian.murrell@intel.com>
2017-05-18 16:03:09 -0400

    Remove debug; enable set -e

--- test_inside_docker.sh
+++ test_inside_docker.sh
@@ -1,19 +1,7 @@
-#!/bin/bash -x #e
+#!/bin/bash -xe
 
 OS_VERSION=$1
 
-#pwd
-#id
-#mount
-#df -h
-#ls -l /home
-#ps axf
-#rpm -qa
-cat /etc/passwd
-
-env
-
-#echo -en 'travis_fold:start:yum\\r'
 echo 'travis_fold:start:yum'
 yum -y install git mock rpm-build ed sudo
 echo 'travis_fold:end:yum'
@@ -36,27 +24,18 @@ w
 q
 EOF
 
-eval $(grep ^TRAVIS_COMMIT_RANGE= /manager-for-lustre-dependencies/env)
-eval $(grep ^changed_files= /manager-for-lustre-dependencies/env)
+eval $(grep -e "^TRAVIS_COMMIT_RANGE=" -e "^changed_files=" /manager-for-lustre-dependencies/env)
 
 useradd mocker
 usermod -a -G mock mocker
 
-echo "travis_fold:start:groups"
-cat /etc/group
-echo "travis_fold:end:groups"
-
 # pretend python-nose is a changed dir and try to build it
 SUBDIR=python-nose
 su - mocker <<EOF
-set -x
+set -xe
 cd /manager-for-lustre-dependencies/$SUBDIR
 rpmbuild -bs --define epel\ 1 --define _srcrpmdir\ \$PWD --define _sourcedir\ \$PWD *.spec
 echo "travis_fold:start:mock"
 mock \$PWD/*.src.rpm
 echo "travis_fold:end:mock"
 EOF
-
-echo "travis_fold:start:journalctl"
-journalctl
-echo "travis_fold:end:journalctl"
